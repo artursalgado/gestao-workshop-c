7f38675d4e3dcbb377508d787fa8d6f5f7b1aa63
Artur Salgado <artursalgado00@gmail.com>
2026-05-14 19:22:38 +0100

    add struct Apresentacao e lista ligada NoApresentacao

---
+++ include/apresentacao.h
@@ -0,0 +1,21 @@
+#ifndef APRESENTACAO_H
+#define APRESENTACAO_H
+
+typedef struct {
+  int id;
+  char titulo[100];
+  char orador[100];
+  char curriculo[500];
+  char palavras_chave[5][50];
+  int duracao; // em minutos
+  float preco;
+  char data[20]; // data/hora do escalonamento
+  int num_inscritos;
+} Apresentacao;
+
+typedef struct NoApresentacao {
+  Apresentacao dados;
+  struct NoApresentacao *proximo;
+} NoApresentacao;
+
+#endif
